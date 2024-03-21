25ff01f92e70eb4a559b7457ea9c25c8e49f5f2b
NiklasSchw <schwardnik@yahoo.de>
2024-03-21 17:55:03 +0100

    Usericon --> x-Icon

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
-var map = L.map('map').setView([-44.116667, 170.166667], 13);
+var map = L.map('map').setView([-44.046667, 170.166667], 11);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
-var marker = L.marker([-44.116667, 170.166667]).addTo(map);
-marker.bindPopup("<b>Lake Pukaki</b>").openPopup();
+var marker = L.marker([-44.046667, 170.166667]).addTo(map);
+marker.bindPopup("<b>Lake Pukaki</b><p>Neuseeland</p>").openPopup();
 circle.bindPopup("I am a circle.");
 polygon.bindPopup("I am a polygon.");
