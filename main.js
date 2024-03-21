91372119d835ed06778f4d042735885e81bcd96b
NiklasSchw <schwardnik@yahoo.de>
2024-03-21 17:40:09 +0100

    JavaScriptcode für die Karte

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 var map = L.map('map').setView([47.416604, 10.979589], 13);
+
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
+
 var marker = L.marker([47.416604, 10.979589]).addTo(map);
 marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
 circle.bindPopup("I am a circle.");
