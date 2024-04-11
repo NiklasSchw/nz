8d5cae95a29e136078ec54c0cdd852ab5c00b222
NiklasSchw <schwardnik@yahoo.de>
2024-04-11 15:55:51 +0200

    var durch let ersetzt

--- main.js
+++ main.js
@@ -1,11 +1,10 @@
-var map = L.map('map').setView([-44.046667, 170.166667], 11);
+let map = L.map('map').setView([-44.046667, 170.166667], 11);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
-var marker = L.marker([-44.046667, 170.166667]).addTo(map);
+let marker = L.marker([-44.046667, 170.166667]).addTo(map);
 marker.bindPopup("<b>Lake Pukaki</b><p>Neuseeland</p>").openPopup();
-circle.bindPopup("I am a circle.");
-polygon.bindPopup("I am a polygon.");
+
