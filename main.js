fa7bcf7a25e356fbc56b4b28d73797e005ad0632
NiklasSchw <schwardnik@yahoo.de>
2024-04-11 16:12:54 +0200

    Popup als template string mit lat/lng und Titel

--- main.js
+++ main.js
@@ -1,14 +1,20 @@
 let lat = -44.046667;
 let lng = 170.166667;
-let Zoom = 19
+let zoom = 11
 
-let map = L.map('map').setView([lat, lng], 11);
+let map = L.map('map').setView([lat, lng], zoom);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: Zoom,
+    maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
 let marker = L.marker([lat, lng]).addTo(map);
-marker.bindPopup("<b>Lake Pukaki</b><p>Neuseeland</p>").openPopup();
+marker.bindPopup(`
+    <h2>Lake Pukaki</h2>
+    <ul>
+        <li>Breite: ${lat}</li>
+        <li>Länge: ${lng}</li>
+    </ul>
+`).openPopup();
 
