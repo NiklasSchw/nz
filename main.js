2574922e8fa538161c3a0549dd45e49d2b800aae
NiklasSchw <schwardnik@yahoo.de>
2024-04-11 16:05:42 +0200

    Variablen let, lat, zoom

--- main.js
+++ main.js
@@ -1,10 +1,14 @@
-let map = L.map('map').setView([-44.046667, 170.166667], 11);
+let lat = -44.046667;
+let lng = 170.166667;
+let Zoom = 19
+
+let map = L.map('map').setView([lat, lng], 11);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
+    maxZoom: Zoom,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
-let marker = L.marker([-44.046667, 170.166667]).addTo(map);
+let marker = L.marker([lat, lng]).addTo(map);
 marker.bindPopup("<b>Lake Pukaki</b><p>Neuseeland</p>").openPopup();
 
