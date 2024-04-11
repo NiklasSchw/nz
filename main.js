f310b934b6e4c54d159316eac3d9b08d7ee27c7a
NiklasSchw <schwardnik@yahoo.de>
2024-04-11 16:32:32 +0200

    mpa-Aufruf an Dokumentation angepasst

--- main.js
+++ main.js
@@ -2,7 +2,10 @@ let lat = -44.046667;
 let lng = 170.166667;
 let zoom = 11
 
-let map = L.map('map').setView([lat, lng], zoom);
+let map = L.map('map', {
+    center: [lat, lng],
+    zoom: zoom
+});
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
